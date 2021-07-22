d32f829a59980725c5b6ef93f0f348c08b3b40bf
id2k1149 <id2k1149@gmail.com>
2021-07-21 20:55:54 -0400

    add config and controller, delete xml

--- src/main/java/org/id2k1149/controller/HelloController.java
+++ src/main/java/org/id2k1149/controller/HelloController.java
@@ -1,4 +1,4 @@
-package org.id2k1149;
+package org.id2k1149.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello-world")
     public String sayHello() {
-        return "hello_world";
+        return "hello-world";
     }
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>SURVEY</h2>
+<a href="${pageContext.request.contextPath}/hello-world">hi</a>
 </body>
 </html>
