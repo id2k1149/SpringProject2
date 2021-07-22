e39c75f9ff9aa672823099b6735fa1788ee708b5
id2k1149 <id2k1149@gmail.com>
2021-07-22 11:10:24 -0400

    v02 requests

--- src/main/java/org/id2k1149/controllers/FirstController.java
+++ src/main/java/org/id2k1149/controllers/FirstController.java
@@ -3,15 +3,33 @@ package org.id2k1149.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 //@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
+
+    //  #2 @RequestParam("***")
+    @GetMapping("/hello2")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+        return "first/hello2";
+    }
+
+
 
     @GetMapping("/goodbye")
     public String goodByePage() {
